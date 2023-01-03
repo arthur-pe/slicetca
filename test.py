b30f7298cc017f2e26ba44d69c35ca03b8bcae0d
arthur <pellegrino.arthur@gmail.com>
2023-01-03 08:42:49 +0100

    added readme

--- test.py
+++
@@ -1,9 +0,0 @@
-from slicetca import *
-import torch
-
-a = torch.randn(10,20,30)
-
-_, _ = decompose(a, [2,3,4], max_iter=100)
-
-if __name__=='__main__':
-    _, _ = grid_search(a, [2,3,4], max_iter=10)
