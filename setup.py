65cddc99af9c19d6d82d02ad488879e4e003a4c5
arthur <pellegrino.arthur@gmail.com>
2023-03-06 12:47:15 +0100

    -pypi stuff added

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name='slicetca',
+    version='0.1.0',
+    description='Package to perform Slice Tensor Component Analysis',
+    url='https://github.com/arthur-pe/slicetca',
+    author='Arthur Pellegrino',
+    license='MIT',
+    packages=['slicetca'],
+    install_requires=['torch',
+                      'numpy',
+                      'matplotlib',
+                      'tqdm'
+                      ],
+    python_requires='>=3.8',
+    classifiers=[
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
