47c67674fbe370c9a951721105277bdabbdd842b
arthur <pellegrino.arthur@gmail.com>
2023-07-03 20:40:38 +0200

    -grid_search : tqdm high memory usage

--- slicetca/run/grid_search.py
+++ slicetca/run/grid_search.py
@@ -61,9 +61,10 @@ def grid_search(data: Union[torch.Tensor, np.array], #Only works with torch.Tens
     out_grid = []
     with Pool(max_workers=processes_grid) as pool:
         iterator = tqdm(pool.map(dec, grid), total=torch.tensor(grid).size()[0])
+        iterator.set_description('Number of components (completed): ', refresh=True)
         for i, p in enumerate(iterator):
             out_grid.append(p)
-            iterator.set_description('Number of components: '+str(np.unravel_index(i, tuple(max_ranks))), refresh=True)
+            iterator.set_description('Number of components (completed): '+str(np.unravel_index(i, tuple(max_ranks))), refresh=True)
     out_grid = np.array(out_grid, dtype=np.float32)
 
     loss_grid = out_grid[:,0]
