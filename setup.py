e4258728a6d0ccd7c23d28f9e78306f050103188
arthur <pellegrino.arthur@gmail.com>
2023-08-22 15:54:28 +0200

    New version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (this_directory / "README.md").read_text()
 setup(
     name='slicetca',
     packages=find_packages(exclude=['tests*']),
-    version='0.1.9',
+    version='0.1.10',
 
     description='Package to perform Slice Tensor Component Analysis',
     long_description=long_description,
